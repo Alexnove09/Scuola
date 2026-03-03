03d38bbef6bda6c906771434b6de7bd0671333ac
Alexnove09 <150546243+Alexnove09@users.noreply.github.com>
2026-03-03 09:31:36 +0100

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+    public Frame() {
+
+        setTitle("Esempio");
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+
+    }
+}
